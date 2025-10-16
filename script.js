f36b5740a75d067342e238ce0b5394a8d189fd70
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2025-10-16 19:57:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const gridSection = document.getElementsByClassName('grid-section');
+const contato = document.getElementsByClassName('contato');
+
+console.log(gridSection); // Retorna uma HTMLCollection com os elementos de classe "grid-section"
+console.log(gridSection[0]); // Retorna o primeiro elemento da lista
